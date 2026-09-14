f88422731c81e2512196d7f2713049995ec5b7e2
olesia <infocooll.col@gmail.com>
2026-09-14 22:46:31 +0200

    Added the current location

--- src/script.js
+++ src/script.js
@@ -49,16 +49,18 @@ function updateCity(event) {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
+
   let citisElement = document.querySelector("#cities");
-  setInterval(1000);
-  citisElement.innerHTML = `<div class="city">
+  setInterval(() => {
+    let cityTime = moment().tz(cityTimeZone);
+    citisElement.innerHTML = `<div class="city">
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format("h:mm [<small>]:ss A[</small>]")}</div>
         </div>`;
+  }, 1000);
 }
 updateTime();
 setInterval(updateTime, 1000);
