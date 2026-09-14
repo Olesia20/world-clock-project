568e9845a29d8952c1b0f5291f197de65e759c10
olesia <infocooll.col@gmail.com>
2026-09-14 20:38:28 +0200

    added js world time

--- src/script.js
+++ src/script.js
@@ -1,1 +1,46 @@
 //  Europe/Paris Europe/London America/New_York Asia/Singapore
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function setClock() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondDegrees = seconds * 6;
+  const minuteDegrees = minutes * 6;
+  const hourDegrees = hours * 30 + minutes * 0.5;
+
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setClock();
+setInterval(setClock, 1000);
+
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm[<small>]:ss[</small>] [<small>]A[</small>]",
+  );
+  let kyivElement = document.querySelector("#kyiv");
+  let kyivDateElement = kyivElement.querySelector(".date");
+  let kyivTimeElement = kyivElement.querySelector(".time");
+  let kyivTime = moment().tz("Europe/Kiev");
+  kyivDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  kyivTimeElement.innerHTML = kyivTime.format(
+    "h:mm[<small>]:ss[</small>] [<small>]A[</small>]",
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
